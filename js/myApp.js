a29e5ef1dae0d2569e2d21cab9b4c1274a4a5ce6
Anna Valkov <anna.a.valkov@gmail.com>
2015-10-20 19:52:56 -0700

    add controller to ng-madlibs

---
+++ js/myApp.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+	.controller('MyCtrl', function($scope){
+		$scope.maleName = "";
+		$scope.jobTitle = "";
+		$scope.tediousTask = "";
+		$scope.dirtyTask = "";
+		$scope.celebrity = "";
+		$scope.uselessSkill = "";
+		$scope.obnoxiuousCelebrity = "";
+		$scope.hugeNumber = "";
+		$scope.adjective = "";
+	});
