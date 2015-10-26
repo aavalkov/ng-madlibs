e2f5c569a3e3975bb181ce24ba8933783ab83e93
Anna Valkov <anna.a.valkov@gmail.com>
2015-10-25 17:17:29 -0700

    refactored app to add form

--- js/myApp.js
+++ js/myApp.js
@@ -9,4 +9,28 @@ angular.module('myApp', [])
 		$scope.obnoxiuousCelebrity = "";
 		$scope.hugeNumber = "";
 		$scope.adjective = "";
+		$scope.madLibsForm = "";
+		$scope.showForm = true;
+		$scope.submit = function(){
+			if ($scope.madLibsForm.$valid){
+				console.log('The form is valid');
+				$scope.showForm = false;
+			} else {
+				console.log('The form is invalid');
+			}
+		};
+		$scope.startOver = function(){
+			$scope.data.maleName = "";
+			$scope.data.jobTitle = "";
+			$scope.data.tediousTask = "";
+			$scope.data.dirtyTask = "";
+			$scope.data.celebrity = "";
+			$scope.data.uselessSkill = "";
+			$scope.data.obnoxiuousCelebrity = "";
+			$scope.data.hugeNumber = "";
+			$scope.data.adjective = "";
+			$scope.data.madLibsForm = "";
+			$scope.madLibsForm.$setPristine();
+			$scope.showForm = true;
+		}
 	});
